5a35599e87fdc82efe9b2f029f63b4f4776fcc91
hellosamm <142949327+hellosamm@users.noreply.github.com>
2025-08-27 14:31:15 -1000

    updates all around

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -7,6 +7,17 @@ import React from "react";
 const Projects = () => {
   const [expandedIndex, setExpandedIndex] = useState<number | null>(null);
 
+  const getColorForLanguage = (lang: string) => {
+    const colors: Record<string, string> = {
+      JavaScript: "#FDF68C",
+      React: "#1749DF",
+      Rails: "#FDF68C",
+      Python: "#D069E0",
+    };
+
+    return colors[lang];
+  };
+
   const projects = [
     {
       title: "COOKBOOK CLUB",
@@ -20,16 +31,24 @@ const Projects = () => {
         "Amazon S3",
         "Render",
       ],
-      bgColor: "#F99A00",
-      textColor: "#FFFFFF",
+      bgColor: "#D069E0",
+      titleColor: "#FFF",
+    },
+    {
+      title: "PERSONAL SITE",
+      description:
+        "When I wanted an easier way to find recipes using ingredients I already had in my kitchen, I wrote a Python script to scrape data from my favorite recipe site and built a search feature that lets me search by ingredient, helping plan meals and reduce food waste",
+      languages: ["Python", "React"],
+      bgColor: "#1749DF",
+      titleColor: "#FFF",
     },
     {
       title: "INGREDIENT FINDER",
       description:
         "When I wanted an easier way to find recipes using ingredients I already had in my kitchen, I wrote a Python script to scrape data from my favorite recipe site and built a search feature that lets me search by ingredient, helping plan meals and reduce food waste",
       languages: ["Python", "React"],
-      bgColor: "#F99A00",
-      textColor: "#FBCE01",
+      bgColor: "#2F9B50",
+      titleColor: "#FFF",
     },
   ];
 
@@ -48,14 +67,21 @@ const Projects = () => {
           onClick={() => toggleExpand(index)}
           style={{
             backgroundColor: project.bgColor,
-            color: project.textColor,
           }}
         >
-          <h3 className={styles.title}>{project.title}</h3>
+          <h3
+            className={styles.title}
+            style={{
+              color: project.titleColor,
+            }}
+          >
+            {project.title}
+          </h3>
+
           {expandedIndex === index && (
             <div className={styles.details}>
-              <p>{project.description}</p>
               <p>Languages: {project.languages.join(", ")}</p>
+              <p>{project.description}</p>
             </div>
           )}
         </div>
