6cb5f69268602df87b76d4287705b9f10699904c
hellosamm <142949327+hellosamm@users.noreply.github.com>
2025-09-04 15:37:16 -1000

    updated homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="sidebar"></div>
       <main className={style.main}>
         <div className={style.about}>
-          <p>{`hi, I'm sam`}</p>
+          <h1>{`hi, I'm sam`}</h1>
           <p>
             {`I believe we are all born to create. Whether it’s drawing, cooking,
             coding or simply the way we move through the world, making things
@@ -22,23 +22,46 @@ export default function Home() {
             {`I am energized by starting new projects and love to share what I
             know with others. Let’s make some magic!`}
           </p>
-          {/* <img src="/zigzag.svg" alt="" className={style.drawnLine} /> */}
+
+          <div>
+            <h2>Most Recent</h2>
+            <p>
+              my latest{" "}
+              <a href="/coding-projects" className={style.link}>
+                coding projects
+              </a>
+            </p>
+
+            <h2>Thinking About</h2>
+            <p>
+              random things{" "}
+              <a href="/coding-projects" className={style.link}>
+                on my mind
+              </a>
+            </p>
+
+            <h2>What I'm Doing</h2>
+            <ul>OUTDOORS → training for a marathon</ul>
+            <ul>MAKING → a quilt</ul>
+            <ul>COOKING → homemade ice cream & pizza on the grill</ul>
+          </div>
+          <img src="/zigzag.svg" alt="" className={style.drawnLine} />
         </div>
-        {/* <div className={style.btnContainer}>
-          <div className={style.codingBtn}>
+        <div className={style.btnContainer}>
+          {/* <div className={style.codingBtn}>
             <CodingBtn />
-          </div>
-          <div className={style.ommBtn}>
+          </div> */}
+          {/* <div className={style.ommBtn}>
             <OnMyMindBtn />
-          </div>
-          <div className={style.contactBtn}>
+          </div> */}
+          {/* <div className={style.contactBtn}>
             <ContactBtn />
-          </div>
-          <div className={style.section}>
+          </div> */}
+          {/* <div className={style.section}>
             <h3>THINGS I'VE MADE</h3>
             <img src="./red-flower.svg" alt="" />
-          </div>
-        </div> */}
+          </div> */}
+        </div>
       </main>
       <footer></footer>
     </div>
